5352e3fa7f3373bf7c6458b006789721a2db0b8f
borrascador <roflbot101@gmail.com>
2017-02-20 19:33:15 -0500

    added live terminal tweet readout

--- ossp-bot.py
+++ ossp-bot.py
@@ -1,12 +1,6 @@
 import tweepy, time
 from markov import Markov
-from passwords import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, \
-ACCESS_SECRET
-
-KEY = CONSUMER_KEY
-KEY_SECRET = CONSUMER_SECRET
-TOKEN = ACCESS_KEY
-TOKEN_SECRET = ACCESS_SECRET
+from secret import *
 
 auth = tweepy.OAuthHandler(KEY, KEY_SECRET)
 auth.set_access_token(TOKEN, TOKEN_SECRET)
@@ -18,4 +12,5 @@ while True:
     line = markov.generate_markov_text()
     if len(line)>140: line = line[:140]
     api.update_status(line)
+    print(line + " @ " + str(time.ctime()))
     time.sleep(1800) #Tweet every 30 minutes
